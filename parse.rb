048bf37e87294f038bcfae2ac9c05ca0a9600141
hpetrie <hans.petrie@lookout.com>
2016-01-27 08:38:37 -0800

    parsing is working

--- parse.rb
+++ parse.rb
@@ -2,5 +2,18 @@
 
 require 'csv'
 
-parsed = CSV.read("questions.csv")
-puts parsed
+totalrows=0
+questions=Array.new
+
+CSV.foreach('questions.csv') do |row|
+  totalrows=totalrows+1
+  questions.push row
+  #puts row.inspect
+  #puts row[0]
+end
+
+questions.each do |question|
+  puts question
+end
+
+puts "Parsed " + totalrows.to_s + " questions"
