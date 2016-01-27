53afadffa27fda389bb0f0eb53b5ee0ca477bd22
hpetrie <hans.petrie@lookout.com>
2016-01-27 09:06:23 -0800

    Randomizing works but output is not done

--- test.rb
+++ test.rb
@@ -19,6 +19,10 @@ end
 #puts "Parsed " + totalrows.to_s + " questions"
 
 for i in 1..iQuestions do
-  puts questions[i]
+  question_to_ask=rand(1..totalrows-1)
+  puts question_to_ask
+  questions[question_to_ask].each do |question|
+    puts question.inspect
+#    puts questions[question_to_ask]
+  end
 end
-
