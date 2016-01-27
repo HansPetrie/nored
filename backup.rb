abd9b419c16bef6e6b77def0b4693e3ea058240d
hpetrie <hans.petrie@lookout.com>
2016-01-27 08:17:21 -0800

    first commit

---
+++ backup.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+
+puts "Enter the number of questions for the quiz"
+sInput = gets
+iQuestions = sInput.to_i
+puts "You will be given " + iQuestions.to_s + " questions"
---
+++ parse.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+
+require 'csv'
+
+parsed = CSV.read("questions.csv")
+puts parsed
